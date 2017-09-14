7e85fc07a24d8d37eab9fe681a37346487bcb2c3
Ming Qu <ming.qu0202@gmail.com>
2017-09-14 09:36:04 +0200

    image modal
    image modal

--- js/asknow.js
+++ js/asknow.js
@@ -81,9 +81,15 @@ $(".all-questions").prepend(ToC);
 
 
 function myfunction(){
-    alert('hello');
-    var el = document.getElementById('test');
-    el.parentNode.removeChild(el);
+    const list = document.getElementsByClassName('listImg');
+
+    for (var i = 0; i < list.length; i++){
+        alert(list.length)
+
+        list[i].parentNode.removeChild(list[i]);
+    }
+
+
     var newitem = "<div class='list-item'>" +
         "          <div class='list-content'>" +
         "            <h2>Hamburg</h2>" +
@@ -92,45 +98,4 @@ function myfunction(){
         "          </div>" +
         "        </div>"
     $(".list").append(newitem);
-
-
-}
-
-
-//image gallery
-function openModal() {
-    document.getElementById('myModal').style.display = "block";
-}
-
-function closeModal() {
-    document.getElementById('myModal').style.display = "none";
-}
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("demo");
-    var captionText = document.getElementById("caption");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-    captionText.innerHTML = dots[slideIndex-1].alt;
 }
