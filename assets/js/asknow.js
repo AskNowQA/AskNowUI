d405dc39ea232b3c54b3e98c55944d6e4b29aebe
Ming Qu <ming.qu0202@gmail.com>
2017-09-23 22:01:13 +0200

    change home page
    change home page, layout

---
+++ assets/js/asknow.js
@@ -0,0 +1,86 @@
+﻿(function ($) {
+    "use strict";
+    var mainApp = {
+
+        main_fun: function () {
+            //SCROLL SCRIPT
+            $(function () {
+                $('.move-me a').bind('click', function (event) { //just pass move-me in design and start scrolling
+                    var $anchor = $(this);
+                    $('html, body').stop().animate({
+                        scrollTop: $($anchor.attr('href')).offset().top
+                    }, 1000, 'easeInOutQuad');
+                    event.preventDefault();
+                });
+            });
+		//scrollReveal scripts
+            window.scrollReveal = new scrollReveal();
+        },
+
+        initialization: function () {
+            mainApp.main_fun();
+
+        }
+
+    }
+    // Initializing ///
+
+    $(document).ready(function () {
+        mainApp.main_fun();
+    });
+
+}(jQuery));
+
+
+//start resource
+var ToC =
+    "<nav role='navigation' class='table-of-contents'>" +
+    "<h2>Content:</h2>" +
+    "<ul>";
+
+var newLine, el, title, link;
+
+$("#service h4").each(function() {
+
+    el = $(this);
+    title = el.text();
+    link = "#" + el.attr("id");
+
+    newLine =
+        "<li>" +
+        "<a href='" + link + "'>" +
+        title +
+        "</a>" +
+        "</li>";
+
+    ToC += newLine;
+
+});
+
+ToC +=
+    "</ul>" +
+    "</nav>";
+
+$(".all-questions").prepend(ToC);
+
+
+function myfunction(){
+    const list = document.getElementsByClassName('listImg');
+
+    for (var i = 0; i < list.length; i++){
+        alert(list.length)
+
+        list[i].parentNode.removeChild(list[i]);
+    }
+
+    var newitem = "<div class='list-item'>" +
+        "          <div class='list-content'>" +
+        "            <h2>Hamburg</h2>" +
+        "            <p>Bgsm.</p>" +
+        "            <a>Show detailed</a>" +
+        "          </div>" +
+        "        </div>"
+    $(".list").append(newitem);
+}
+
+
---
+++ assets/js/styleSwitcher.js
@@ -0,0 +1,45 @@
+﻿
+(function ($) {
+    "use strict";
+    var mainAppNew = {
+
+        main_fun: function () {
+
+            /*=====================================
+             THEME SWITCHER SCRIPTS 
+            ===================================*/
+            jQuery('#switch-panel').click(function () {
+                if (jQuery(this).hasClass('show-panel')) {
+                    jQuery('.switcher').css({ 'left': '-50px' });
+                    jQuery('#switch-panel').removeClass('show-panel');
+                    jQuery('#switch-panel').addClass('hide-panel');
+                } else if (jQuery(this).hasClass('hide-panel')) {
+                    jQuery('.switcher').css({ 'left': 0 });
+                    jQuery('#switch-panel').removeClass('hide-panel');
+                    jQuery('#switch-panel').addClass('show-panel');
+                }
+            });
+
+           
+            $('#green').click(function () {
+                $('#mainCSS').attr('href', 'assets/css/themes/green.css');
+            });
+            $('#red').click(function () {
+                $('#mainCSS').attr('href', 'assets/css/themes/red.css');
+            });
+           
+        },
+
+        initialization: function () {
+            mainAppNew.main_fun();
+
+        }
+
+    }
+    // Initializing ///
+
+    $(document).ready(function () {
+        mainAppNew.main_fun();
+    });
+
+}(jQuery));
