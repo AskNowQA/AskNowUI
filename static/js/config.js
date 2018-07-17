be1b07d3be938605c154b349f7dbe236359fbf5c
liyakun <liyakun127@gmail.com>
2018-07-17 16:30:58 +0200

    connect correct socket server

--- static/js/config.js
+++ static/js/config.js
@@ -1,9 +1,9 @@
-var AppConfig = 
+var AppConfig =
 {
 	"speechRecogSrv":
 	{
 		//"speech": "wss://nm-live-speech.iais.fraunhofer.de:443/worker_only_1/a39d8e0b/ws/speech",
-		"speech": "ws://localhost:5300",
+		"speech": "ws://nm-sprachass:5300",
 		"status": "wss://nm-live-speech.iais.fraunhofer.de:443/worker_only_1/a39d8e0b/ws/status"
 	},
 	"questionAnsSrv":
