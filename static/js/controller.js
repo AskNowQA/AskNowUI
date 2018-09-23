34c94fc4a448a83f084f4b433e3d0816f5a7131b
liyakun <liyakun127@gmail.com>
2018-09-23 13:19:19 +0200

    update css

--- static/js/controller.js
+++ static/js/controller.js
@@ -2,24 +2,27 @@ $( document ).ready(function() {
 	console.log('Starting SpeechRecognition library.');
 	var speech = new Speech();
 
-    speech.recognition.onstart = function() {
+  speech.recognition.onstart = function() {
 		$('#start').val("false");
 		document.getElementById('listen').style.visibility = "visible";
-    }
+  }
 
 	speech.recognition.onend = function() {
 		$('#start').val("true");
-		//updateDialogues($("#question").val());
+		$('#start').addClass('start').removeClass('end');
 		document.getElementById('listen').style.visibility = "hidden";
-    }
+  }
 
 	$('#start').click(function(){
 		if ($('#start').val() == "true") {
-			//document.getElementById("results").innerHTML = "";
+			$("#question").val("");
 			speech.startCapture();
+			$('#start').addClass('end').removeClass('start');
 		}
 		else {
 			speech.stopCapture();
+			$('#start').val("true");
+		  $('#start').addClass('start').removeClass('end');
 		}
 	});
 });
